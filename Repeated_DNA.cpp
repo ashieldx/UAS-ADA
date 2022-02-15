2dd8526f6db9d994f6bf1924610bdb12b8501eb6
ashield <73387100+ashieldx@users.noreply.github.com>
2022-02-15 20:34:59 +0700

    Update Repeated_DNA.cpp

--- Repeated_DNA.cpp
+++ Repeated_DNA.cpp
@@ -2,15 +2,12 @@
 
 using namespace std;
 
-int ctr = 0; // to count number of iterations (check time complexity)
-
 bool searchString(string a, string b){
 	int i = 0, j = 0, pos = 0;
 	int len_a = a.length(), len_b = b.length();
 	bool flag = false;
 		
 	while(j < len_b){ 
-		ctr++; // to count number of iterations (check time complexity)
 		if(a[i] == b[j]){
 			i++, j++;
 			if(i == len_a){ 
